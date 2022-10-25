98bc4d25d6b80b80788f5d96b49865b6da5c68bc
Alejandra Medina <Jesus@DESKTOP-U7R0A85>
2022-10-25 11:07:34 -0700

    Un estado de cuenta

---
+++ src/mx/itson/catrina/entidades/Cliente.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.catrina.entidades;
+
+/**
+ *
+ * @author Jesus
+ */
+public class Cliente {
+    
+    private String nombre;
+    private String domicilio;
+    private String ciudad;
+    private String rfc;
+    private int cp; 
+    private int codigo; 
+    
+}
---
+++ src/mx/itson/catrina/entidades/Movimientos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.catrina.entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Jesus
+ */
+public class Movimientos {
+    private String descripcion;
+    private double cantidad;
+    private Date fecha;
+    
+}
